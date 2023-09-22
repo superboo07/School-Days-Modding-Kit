2bebf732122c8f5618fa63d30eaafdd312af3af7
superboo07 <39241538+superboo07@users.noreply.github.com>
2023-09-22 19:26:55 -0400

    Update Cutscene Randomizer.py
    Fix bug where files randomly disappear lol

--- Cutscene Randomizer.py
+++ Cutscene Randomizer.py
@@ -2,30 +2,36 @@
 # This script assumes you have extracted the games GPK files
 # Please place this script in the script/English folder
 #
-# Scrambles cutscenes so they run completely out of order
-# I've only had the game crash once but I expect that won't be the last
+# Scrambles cutscenes, so they run completely out of order
+# I've only had the game crash once, but I expect that won't be the last
 # The script itself is very buggy atm
 
 import distutils.dir_util as dir_util
 import distutils.file_util as file_util
 import os
 import random
+import time
 
 
 def scrambleDirectory(directory):
     print("Scrambling directory " + directory)
     dirfileList = os.listdir(directory)
-    dirfileList.pop(len(dirfileList) - 1)
+    # I probably don't need this, but I don't know if python allows me to edit
+    # arrays while they are being iterated through in a for loop without it bugging out
     newfileList = dirfileList
-    dir_util.mkpath("tmp/")
+    dir_util.mkpath(".tmp/")
+    randomFiles = list()
     for i in dirfileList:
-        randomIndex = random.randint(0, len(newfileList) - 1)
-        file_util.copy_file(directory + "/" + i, "tmp/")
-        file_util.move_file("tmp/" + i, "tmp/" + newfileList[randomIndex])
-        print(newfileList[randomIndex])
-        newfileList.pop(randomIndex)
+        checking = True
+        while checking:
+            randomFilename = random.choice(newfileList)
+            if not randomFilename in randomFiles:
+                checking = False
+        randomFiles.append(randomFilename)
+        file_util.move_file(directory + i, ".tmp/" + randomFilename)
+        print(i, "is now", randomFilename)
     dir_util.remove_tree(directory)
-    dir_util.copy_tree("tmp/", directory)
+    dir_util.copy_tree(".tmp/", directory)
     dir_util.remove_tree("tmp/")
 
 
@@ -44,7 +50,7 @@ dir_util.remove_tree("./04")
 dir_util.remove_tree("./05")
 fileList = os.listdir("./balls/")
 for i in fileList:
-    folderName = i[:2]
+    folderName = i[0:2]
     dir_util.mkpath("./" + folderName + "/")
-    file_util.move_file("./balls/" + i, "./" + folderName + "/")
+    file_util.copy_file("./balls/" + i, "./" + folderName + "/" + i)
 dir_util.remove_tree("./balls/")
